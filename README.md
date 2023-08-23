0169a6a0cc0ca47a4e24f15a121a7a142e876a19
oddtO <121516833+oddtO@users.noreply.github.com>
2023-08-23 03:58:16 +0300

    add background img for selected section and main content

---
+++ README.md
@@ -0,0 +1,3 @@
+pexels-waldemar-2290070.jpg: Photo by Waldemar from Pexels: https://www.pexels.com/photo/dining-area-of-the-building-with-no-people-2290070/
+
+NicePng_wood-sign-png_98922.webp: <a href="https://www.nicepng.com/ourpic/u2q8a9e6e6e6q8a9_wood-sign-png/" target="_blank">Wood Sign Png @nicepng.com</a>
--- webpack.config.js
+++ webpack.config.js
@@ -30,9 +30,14 @@ module.exports = {
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
     ],
+
   },
   optimization: {
     minimizer: [`...`, new CssMinimizerWebpackPlugin()],
