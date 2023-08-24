6bd22dc8f5394a193ca880fe694cbed25a22e390
oddtO <121516833+oddtO@users.noreply.github.com>
2023-08-24 07:04:45 +0300

    add contact elements

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-import '../css/reset.css'
-import '../css/style.css'
-import '../css/home.css'
-import '../css/menu.css'
+import "../css/reset.css";
+import "../css/style.css";
+import "../css/home.css";
+import "../css/menu.css";
+import "../css/contact.css";
