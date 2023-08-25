41b786e5ddd6884a7f7d4c1447e4d1421a1d4dbf
oddtO <121516833+oddtO@users.noreply.github.com>
2023-08-25 05:12:04 +0300

    release
    release

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,54 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
-  entry: "./src/js/index.js",
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode == "production";
+  const config = {
+    entry: "./src/js/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
+      clean: true,
+    },
 
-  mode: "development",
 
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-    watchFiles: ["src/**/*", "public/**/*"],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "./src/index.html" }),
-    new MiniCssExtractPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
+    mode: argv.mode,
+    devtool: isProduction ? undefined : "inline-source-map",
+    devServer: {
+      static: "./dist",
+      watchFiles: ["src/**/*", "public/**/*"],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "./src/index.html" }),
+      new MiniCssExtractPlugin(),
     ],
-  },
-  optimization: {
-    minimizer: [`...`, new CssMinimizerWebpackPlugin()],
-  },
-}
+    module: {
+      rules: [
+        {
+          test: /\.css/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [`...`, new CssMinimizerWebpackPlugin()],
+      minimize: isProduction,
+      usedExports: true,
+    },
+  };
+
+  return config;
+};
